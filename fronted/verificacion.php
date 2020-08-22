76b9bb2a2bfa944384b6540967d064d664ffa4d1
allanumanzor55 <68833929+allanumanzor55@users.noreply.github.com>
2020-08-22 14:51:06 -0600

    Allan Alonzo
    Mas Problemas Del Commit

--- fronted/verificacion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('../backend/class/class-usuarios.php');
-    require_once('../backend/class/class-database.php');
-    $database = new Database();
-    $db = $database->getDB();
-    if(!Usuarios::verificarAutentificacion($db)){
-        header("Location: error.php");
-    }
-?>
